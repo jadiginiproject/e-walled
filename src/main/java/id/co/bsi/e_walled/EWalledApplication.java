c2b7e895c7db12b55bb8a6e9228db406d583e22e
jeeshakira <shakirapratiwi0507@gmail.com>
2025-04-16 11:17:33 +0700

    e-walled

---
+++ src/main/java/id/co/bsi/e_walled/EWalledApplication.java
@@ -0,0 +1,13 @@
+package id.co.bsi.e_walled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bsi/e_walled/EWalledApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bsi.e_walled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
