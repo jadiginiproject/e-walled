6595d7d5520b3f32a417e4a68c7c6df9b85012db
aldinmldn <aldinmldn@gmail.com>
2025-04-17 10:21:39 +0700

    test add model transactions

--- src/main/java/id/co/bsi/e_walled/model/Transactions.java
+++ src/main/java/id/co/bsi/e_walled/model/Transactions.java
@@ -1,4 +1,22 @@
 package id.co.bsi.e_walled.model;
 
+
+import lombok.Data;
+
+import java.util.Date;
+
+//@Data
+//@Entity
+//@Table
 public class Transactions {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private Long wallet_id;
+//    private String transaction_type;
+//    private Long amount;
+//    private Long recipient_wallet_id;
+//    private String sender;
+//    private Date transaction_date;
+//    private String description;
 }
